105f7b8bf855b099939908b3e31350ceba8db681
achaudhary591 <achaudhary591@gmail.com>
2023-05-27 15:27:08 +0530

    library system done

---
+++ src/main/java/com/akshyaChaudhary/librarymanagementsystem/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.akshyaChaudhary.librarymanagementsystem.repository;
+
+import com.akshyaChaudhary.librarymanagementsystem.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ src/main/java/com/akshyaChaudhary/librarymanagementsystem/repository/PublisherRepository.java
@@ -0,0 +1,8 @@
+package com.akshyaChaudhary.librarymanagementsystem.repository;
+
+import com.akshyaChaudhary.librarymanagementsystem.entity.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+
+}
